d8693520fb4e5a997e4c5df193cb3e8f83178ee8
Sujil Anto <sujilnt@gmail.com>
2018-03-24 02:52:17 +0000

    adding new dist

--- dist/script.js
+++ dist/script.js
@@ -88,10 +88,6 @@ var _loadMap = __webpack_require__(3);
 
 var _loadMap2 = _interopRequireDefault(_loadMap);
 
-var _categories = __webpack_require__(4);
-
-var _categories2 = _interopRequireDefault(_categories);
-
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
 var a = "successful ";
@@ -104,20 +100,66 @@ console.log("adding a change", a);
 "use strict";
 
 
+var _categories = __webpack_require__(4);
+
+var _categories2 = _interopRequireDefault(_categories);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
 function initMap() {
-    var google = window.google;
-    var uluru = { lat: 51.4816, lng: -3.1791 };
-    //° N, ° W
-    var map = new google.maps.Map(document.getElementById('map'), {
-        zoom: 14.5,
-        center: uluru
-    });
-    var marker = new google.maps.Marker({
-        position: { lat: 51.484135, lng: -3.169751 },
-        map: map,
-        title: "Cardiff School of Computer Science"
-    });
+    //currentLocation() : - Its basically used to Identify thr User Location , it returns an Object.
+    var currentLocation = function currentLocation() {
+        if (navigator.geolocation) {
+            navigator.geolocation.getCurrentPosition(function (position) {
+                initMapLoad(position, position.coords);
+            });
+        } else {
+            console.log("Geolocation is not supported by this browser.");
+        }
+    };
+    var initMapLoad = function initMapLoad(positionObj, cordinate) {
+        var uluru = {
+            lat: cordinate.latitude,
+            lng: cordinate.longitude
+        };
+        var map = new google.maps.Map(document.getElementById('map'), {
+            zoom: 14,
+            center: uluru
+        });
+        var service = new google.maps.places.PlacesService(map);
+        var request = {
+            location: uluru,
+            radius: '500',
+            type: ['restaurant']
+        };
+        service.nearbySearch(request, callback);
+        function callback(results, status) {
+            if (status == google.maps.places.PlacesServiceStatus.OK) {
+                for (var i = 0; i < results.length; i++) {
+                    // adding markers to each element
+                    createMarker(results[i]);
+                }
+            }
+        }
+        var createMarker = function createMarker(data) {
+            var marker = new google.maps.Marker({
+                map: map,
+                icon: data.image,
+                title: data.name,
+                position: data.geometry.location
+            });
+            var infowindow = new google.maps.InfoWindow();
+            google.maps.event.addListener(marker, 'click', function () {
+                infowindow.setContent(data.name);
+                infowindow.open(map, this);
+            });
+        };
+        var bounds = new google.maps.LatLngBounds();
+    };
+    // find the current location
+    currentLocation();
 }
+
 window.initMap = initMap;
 
 /***/ }),
@@ -126,6 +168,16 @@ window.initMap = initMap;
 
 "use strict";
 
+
+Object.defineProperty(exports, "__esModule", {
+    value: true
+});
+
+var _dataApi = __webpack_require__(5);
+
+var _dataApi2 = _interopRequireDefault(_dataApi);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
 var categoryId = document.getElementById("categories");
 var value = "<div class=\"cat\">Categories</div>\n            <div class=\"flex jc\">\n                <lable class=\"prel\" for=\"pub\" ><input class=\"checkBox\" type=\"checkbox\" value=\"pub\" id=\"pub\" onChanges=\"elementBind(this)\"><label for=\"pub\">pubs</label></lable>\n                <lable class=\"prel\" for=\"restaurants\"><input class=\"checkBox\" type=\"checkbox\" id=\"restaurants\" value=\"restaurants\" ><label for=\"restaurants\">Restaurants</label></lable>\n                <lable class=\"prel\" for=\"clubs\"><input class=\"checkBox\" type=\"checkbox\" id=\"clubs\"><label>clubs</label></lable>\n                <lable class=\"prel\" for=\"library\"><input class=\"checkBox\" type=\"checkbox\" id=\"library\"><label>Library</label></lable>\n            </div>\n";
@@ -149,6 +201,24 @@ function elementBind(e) {
 var fetchingMapQuery = function fetchingMapQuery(arr) {
     console.log("array", arr);
 };
+
+exports.default = fetchingMapQuery();
+
+/***/ }),
+/* 5 */
+/***/ (function(module, exports, __webpack_require__) {
+
+"use strict";
+
+
+var collectingData = function collectingData(data) {
+    console.log("data", data);
+};
+
+var apiCallduringOnCheck = function apiCallduringOnCheck() {};
+
+console.log(apiCallduringOnCheck);
+console.log("called", apiCallduringOnCheck());
 
 /***/ })
 /******/ ]);
